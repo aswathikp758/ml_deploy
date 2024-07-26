0e59f6cad655f753683010d900653d5407a0828b
aswathikp758 <aswathikp758@gmail.com>
2024-07-26 15:24:13 +0530

    first commit

--- diabetes_prediction/settings.py
+++ diabetes_prediction/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-9k(==v^$fg12ngjmcn)m3isn9%%m#4a6c#p7nd6-)_w#)pf0zo"
+#SECRET_KEY = "django-insecure-9k(==v^$fg12ngjmcn)m3isn9%%m#4a6c#p7nd6-)_w#)pf0zo"
+SECRET_KEY=os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG","False").lower()=="true"
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
